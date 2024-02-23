e366b004a1cb991a473dd41d06992c2585efd59c
Adrian <adrian.luque.rod@gmail.com>
2024-02-23 10:44:27 +0100

    tarea4

--- UD03.4. Boletín de ejercicios JUnit/src/test/java/unidad3/tarea4/AllTests.java
+++ UD03.4. Boletín de ejercicios JUnit/src/test/java/unidad3/tarea4/AllTests.java
@@ -4,7 +4,7 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectClasses({ BoaTest.class, FridgeTest.class, PilaTest.class, SubscripcionTest.class })
+@SelectClasses({ BoaTest.class, FridgeTest.class, PilaTest.class, SubscripcionTest.class, AccountTest.class, AccountTest2.class })
 public class AllTests {
 
 }
